f1497627f9d61e733383dbc3d0d3455571273b69
Makson91 <olesyaperelmuter@gmail.com>
2024-03-31 17:24:13 +0300

    third commit

--- src/layout/sections/contacts/Contacts.tsx
+++ src/layout/sections/contacts/Contacts.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import styled from "styled-components";
+import {SectionTitle} from "../../../components/SectionTitle";
 
 export const Contacts = () => {
     return (
         <StyledContacts>
-
+<SectionTitle></SectionTitle>
         </StyledContacts>
     );
 };
